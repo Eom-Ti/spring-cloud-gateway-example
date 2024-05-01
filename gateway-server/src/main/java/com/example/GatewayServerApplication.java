06ee8ca5366b3f672057457d18336c5747e2c890
엄태권 <akfls221@gmail.com>
2024-05-02 03:22:05 +0900

    feat : gateway-server init

---
+++ gateway-server/src/main/java/com/example/GatewayServerApplication.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
